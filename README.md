3087d607f6f22d030f5cae721bb45baf18385083
louismeunier <louismeunier123@gmail.com>
2021-06-21 16:35:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cra-template-louismeunier
+
+A custom CRA template, featuring Typescript, TailwindCSS and Prettier. No tests are included because that's too responsible.
---
+++ template/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-red-500 grid place-items-center">
+      <div className="grid h-2/3 w-2/3 grid-flow-row">
+        <h1>@louismeunier's CRA template</h1>
+        <h2>with Typescript, Prettier, and TailwindCSS</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
